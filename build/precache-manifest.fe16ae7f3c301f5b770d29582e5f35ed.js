7ec075e4e2cbb870bf6d8428910b6c99bb10d0e1
hurdaudio <hurdaudio@gmail.com>
2020-06-28 12:40:05 -0600

    setting new main

---
+++ build/precache-manifest.fe16ae7f3c301f5b770d29582e5f35ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc122cd8b77546c5de018e34d31e6244",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8abb1ddc3d8d48e0970e",
+    "url": "/static/css/main.7c72a0ce.chunk.css"
+  },
+  {
+    "revision": "871d0b13cd714a3680a5",
+    "url": "/static/js/2.69871f86.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.69871f86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8abb1ddc3d8d48e0970e",
+    "url": "/static/js/main.e98371fb.chunk.js"
+  },
+  {
+    "revision": "bbf161b224292633fa66",
+    "url": "/static/js/runtime-main.60f11b9a.js"
+  }
+]);
