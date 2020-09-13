3489ef429a5010b52bfa2cd6e0297d1f50cd78da
hurdaudio <hurdaudio@gmail.com>
2020-09-12 20:52:24 -0600

    landing - stage 1 - data table support

---
+++ migrations/20200912203216_users.js
@@ -0,0 +1,23 @@
+'use strict';
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function(table) {
+    table.uuid('uuid').notNullable().primary();
+    table.string('email').notNullable().unique();
+    table.string('first_name').notNullable().defaultTo('');
+    table.string('last_name').defaultTo('');
+    table.string('hashed_password').notNullable().defaultTo('');
+    table.boolean('is_admin').notNullable().defaultTo(false);
+    table.boolean('email_confirmed').notNullable().defaultTo(false);
+    table.string('avatar_path').notNullable().defaultTo('');
+    table.integer('clock_resolution').notNullable().defaultTo(480);
+    table.uuid('midi_patch').defaultTo(null);
+    table.json('associates').defaultTo(null);
+    table.json('security').defaultTo(null);
+    table.json('email_reset').defaultTo(null);
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
+};
